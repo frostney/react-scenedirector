c0c148e93cf9a0222f97c2fef82f5430e59ca45b
Johannes Stein <johannesstein@freeze-dev.com>
2016-03-31 10:15:35 +0100

    Allows `switchToScene` on `context`

--- src/SceneDirector.js
+++ src/SceneDirector.js
@@ -4,42 +4,79 @@ class SceneDirector extends Component {
   static propTypes = {
     scenes: PropTypes.object,
     initialScene: PropTypes.string,
+    onSwitchScene: PropTypes.func,
+  };
+
+  static childContextTypes = {
+    switchToScene: React.PropTypes.func,
   };
 
   static defaultProps = {
     scenes: {},
-  }
+  };
 
   constructor(props) {
     super(props);
 
     this.state = {
-      currentScene: this.props.initialScene,
+      currentScene: this.getInitialScene(),
     };
   }
+
+  getChildContext() {
+    return { switchToScene: this.switchToScene };
+  }
+
+  componentDidMount() {
+    if (this.areScenesValid()) {
+      if (this.props.onSwitchScene) {
+        this.props.onSwitchScene(this.getInitialScene());
+      }
+    }
+  }
+
+  getInitialScene = () => {
+    if (this.props.initialScene) {
+      return this.props.initialScene;
+    }
+
+    return Object.keys(this.props.scenes)[0];
+  };
+
+  areScenesValid = () => {
+    if (!this.props.scenes || Object.keys(this.props.scenes).length === 0) {
+      return false;
+    }
+
+    if (!this.state.currentScene) {
+      return false;
+    }
+
+    return true;
+  };
 
   switchToScene = scene => {
     this.setState({
       currentScene: scene,
+    }, () => {
+      if (this.props.onSwitchScene) {
+        this.props.onSwitchScene(scene);
+      }
     });
   };
 
   render() {
-    if (!this.state.scenes || Object.keys(this.state.scenes) === 0) {
+    if (!this.areScenesValid()) {
       return null;
     }
 
-    if (!this.state.currentScene) {
-      return this.state.scenes[Object.keys(this.state.scenes)[0]];
-    }
-
-    const scene = this.state.scenes[this.state.currentScene];
+    const scene = this.props.scenes[this.state.currentScene];
 
     if (!scene) {
       return null;
     }
 
-    React.createElement(scene, { switchToScene: this.switchToScene });
+    return React.createElement(scene, { switchToScene: this.switchToScene });
   }
 }
 
